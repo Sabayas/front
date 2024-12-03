a66a82b1f53a0009a52603d5ce62909cf331ed38
Sabayas <sabayasmin345@gmail.com>
2024-12-03 11:20:40 +0530

    Added Product List

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './Popular.css'
-import '../Assets/all_product.js'
-import '../Assets/data.js'
+
 import data_product from '../Assets/data.js'
+import Item from '../Item/Item.jsx'
+
 
 const Popular = () => {
   return (
@@ -10,8 +11,8 @@ const Popular = () => {
         <h1>POPULAR IN WOMEN</h1>
         <hr/>
         <div className="popular-item">
-            {data_product.map((item,i)=>{
-                return <item key ={i} id ={item.id} name ={item.name} image ={item.image} new_price = {item.new} old_price ={item.old_price} />
+            {data_product.map((item, i)=>{
+                return <Item key ={i} id ={item.id} name ={item.name} image ={item.image}  new_price = {item.new_price} old_price ={item.old_price} />
             })}
            
         </div>
