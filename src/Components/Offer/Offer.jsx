ec4bdc4be77f525edd2e31511c7c26b02aa97901
Sabayas <sabayasmin345@gmail.com>
2024-12-04 11:30:42 +0530

    Added NewsLetter

--- src/Components/Offer/Offer.jsx
+++ src/Components/Offer/Offer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../Offer/Offer.css'
+import exclusive_image from '../Assets/exclusive_image.png'
 
 const Offer = () => {
   return (
     <div className='offers'>
+      <div className="offer-left">
+        <h1>Exclusive</h1>
+        <h1>Offers for you</h1>
+        <p>ONLY ON BEST SELLER PRODUCTS</p>
+        <button>Check Now</button>
+              </div>
+      <div className="offer-right">
+        <img src= {exclusive_image } alt="" />
+      </div>
       
     </div>
   )
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular.jsx'
+import Offer from '../Components/Offer/Offer.jsx'
+import NewCollection from '../Components/New-Collection/NewCollection.jsx'
+
 
 const Shop = () => {
   return (
     <div>
       <Hero></Hero>
       <Popular></Popular>
+      <Offer></Offer>
+      <NewCollection></NewCollection>
+      
       
     </div>
   )
