6fad5cd41525e5c44ed1fc71b85cce960495a91b
Sabayas <sabayasmin345@gmail.com>
2024-12-05 13:55:15 +0530

    Added Context

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import Product from './Pages/Product.jsx';
 import Cart from './Pages/Cart.jsx';
 import LoginSignup from './Pages/LoginSignup.jsx';
 import Footer from './Components/Footer/Footer.jsx';
-
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -18,9 +20,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path= '/' element ={<Shop></Shop>}/>
-        <Route path='/mens' element ={<ShopCategory category ="men"/>}/>
-        <Route path='/women' element ={<ShopCategory category ="women"/>}/>
-        <Route path='/kids' element ={<ShopCategory category ="kid"/>}/>
+        <Route path='/men' element ={<ShopCategory banner ={men_banner} category ="men"/>}/>
+        <Route path='/women' element ={<ShopCategory banner ={women_banner} category ="women"/>}/>
+        <Route path='/kid' element ={<ShopCategory banner ={kid_banner}category ="kid"/>}/>
         <Route path='/product' element ={<Product></Product>}/>
         
       </Routes>
